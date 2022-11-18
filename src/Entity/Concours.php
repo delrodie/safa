3e72bf2acb79426c2f06919ca936571e3fd78dd1
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2022-11-18 16:24:22 +0000

    Gestion des Concours

---
+++ src/Entity/Concours.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ConcoursRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ConcoursRepository::class)]
+class Concours
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $code = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $debut = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $fin = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $slug = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(?string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getDebut(): ?\DateTimeInterface
+    {
+        return $this->debut;
+    }
+
+    public function setDebut(?\DateTimeInterface $debut): self
+    {
+        $this->debut = $debut;
+
+        return $this;
+    }
+
+    public function getFin(): ?\DateTimeInterface
+    {
+        return $this->fin;
+    }
+
+    public function setFin(?\DateTimeInterface $fin): self
+    {
+        $this->fin = $fin;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(?string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
--- src/Service/Utility.php
+++ src/Service/Utility.php
@@ -2,10 +2,18 @@
 
 namespace App\Service;
 
+use App\Repository\ConcoursRepository;
 use Symfony\Component\String\Slugger\AsciiSlugger;
 
 class Utility
 {
+    private ConcoursRepository $concoursRepository;
+
+    public function __construct(ConcoursRepository $concoursRepository)
+    {
+        $this->concoursRepository = $concoursRepository;
+    }
+
     /**
      * @param $string
      * @return \Symfony\Component\String\AbstractUnicodeString
@@ -14,5 +22,23 @@ class Utility
     {
         $slugify = new AsciiSlugger();
         return $slugify->slug(strtolower($string));
+    }
+
+    /**
+     * Generation du code de concours
+     *
+     * @param $entity
+     * @return mixed
+     */
+    public function codeConcours($entity): mixed
+    {
+        $verif = $this->concoursRepository->findOneBy([],['id'=>"DESC"]);
+        if (!$verif) $code = 1;
+        else $code = (int) $verif->getId() +1;
+
+        $reference = "S".$code;
+        $entity->setCode($reference);
+
+        return $entity;
     }
 }
