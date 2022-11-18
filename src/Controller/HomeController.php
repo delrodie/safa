f8c8c3ba37049b319b222c9365c72b2256863963
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2022-11-18 20:47:55 +0000

    Gestion Famille frontend

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\FamilleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private FamilleRepository $familleRepository;
+
+    public function __construct(FamilleRepository $familleRepository)
+    {
+        $this->familleRepository = $familleRepository;
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'familles' => $this->familleRepository->findAll(),
         ]);
     }
 }
