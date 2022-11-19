6abad897f36b2df6f8e2f778ba3c77b58f1ae920
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2022-11-19 12:36:27 +0000

    Gestion des recherches

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\FamilleRepository;
 use App\Service\Utility;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,6 +25,20 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig', [
             'familles' => $this->utility->listFamilleByConcoursActif(),
+        ]);
+    }
+
+    #[Route('/recherche', name: 'app_recherche', methods:['GET','POST'])]
+    public function recherche(Request $request, FamilleRepository $familleRepository): Response
+    {
+        $couple = $request->request->get('_recherche');
+        if ($couple and $request->request->get('_csrf_token')) { //dd($couple);
+            $famille = $familleRepository->findOneBy(['nom' => $couple]); //dd($famille);
+            return $this->redirectToRoute('app_vote_show',['slug' => $famille->getSlug()]);
+        }
+
+        return $this->render('home/recherche.html.twig',[
+            'familles' => $familleRepository->findByConcoursActif()
         ]);
     }
 }
