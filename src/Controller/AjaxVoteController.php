4b81c8f42bd1b044689d7d39259e21f4dc0d0c01
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2022-11-25 20:46:05 +0000

    liste vote

--- src/Controller/AjaxVoteController.php
+++ src/Controller/AjaxVoteController.php
@@ -23,11 +23,11 @@ class AjaxVoteController extends AbstractController
         $this->utility = $utility;
     }
 
-    #[Route('/', name: 'app_ajax_anomalie')]
+    /*#[Route('/', name: 'app_ajax_anomalie')]
     public function anomalie(): JsonResponse
     {
         return $this->json($this->utility->listAnomalie());
-    }
+    }*/
 
     #[Route('/{slug}', name: 'app_ajax_vote', methods: ['GET'])]
     public function index(Request $request, Concours $concours): JsonResponse
