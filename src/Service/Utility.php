14e0af2fb08ee7e497fad072933f140416d27abb
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2022-11-25 20:23:02 +0000

    Anomalie

--- src/Service/Utility.php
+++ src/Service/Utility.php
@@ -273,8 +273,8 @@ class Utility
 
         //if (!$votes) return false;
 
-        foreach ($votes as $vote){
-            if (!is_numeric($vote->getTelephone())){ dd($vote);
+        foreach ($votes as $vote){dd($vote);
+            if (!is_numeric($vote->getTelephone())){
                 // enregistrer le vote contenant anomalie dans la table anomalie
                 $anomalie = new Anomalie();
                 $anomalie->setTelephone($vote->getTelephone());
